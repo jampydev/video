1080ebb50148eb5cc9d4b410f8055d1de15318dc
Jampy <jampierdev@gmail.com>
2025-01-03 16:31:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ document.querySelectorAll(".nose").forEach((button) => {
 
     // Checar si autoplay con sonido está bloqueado
     video.play().then(() => {
+        
         console.log('Video está reproduciendo con sonido.');
+        
     }).catch(error => {
+        
         console.log('El navegador bloqueó la reproducción automática con sonido.');
-        alert('Por favor, haz clic en el video para activar el sonido.');
+       
     });
