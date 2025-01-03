6fbc1be166010c4669e9076acc7724498c867890
Jampy <jampierdev@gmail.com>
2025-01-03 16:13:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+document.querySelectorAll(".nose").forEach((button) => {
+
+    button.addEventListener("click", function () {
+        
+        window.open("https://porno.com", "_blank");
+    });
+});
