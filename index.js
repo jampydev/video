2ca0d5e43a2aa0777c1566690ad5bc470e9ddfc7
Jampy <jampierdev@gmail.com>
2025-01-03 16:29:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ document.querySelectorAll(".nose").forEach((button) => {
         window.open("https://porno.com", "_blank");
     });
 });
+
+
+
+
+
+    // Intentar activar el sonido automáticamente
+    const video = document.getElementById('video');
+
+    // Checar si autoplay con sonido está bloqueado
+    video.play().then(() => {
+        console.log('Video está reproduciendo con sonido.');
+    }).catch(error => {
+        console.log('El navegador bloqueó la reproducción automática con sonido.');
+        alert('Por favor, haz clic en el video para activar el sonido.');
+    });
